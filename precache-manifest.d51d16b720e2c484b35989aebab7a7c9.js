f4670153a3b3ab01cfb359f0ee446cb0f06b9dfc
hossain1527 <hossain.sci2015@gmail.com>
2020-10-23 03:14:51 +0100

    Updates

---
+++ precache-manifest.d51d16b720e2c484b35989aebab7a7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b605e218a93a9cc38bf29bf43b5f8907",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "d2a4672bc5b608dc89c7",
+    "url": "/React-ToDo/static/css/main.fd2225ea.chunk.css"
+  },
+  {
+    "revision": "3b6a3826281970818b7f",
+    "url": "/React-ToDo/static/js/2.ed7b92e2.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/React-ToDo/static/js/2.ed7b92e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a4672bc5b608dc89c7",
+    "url": "/React-ToDo/static/js/main.e5498ccf.chunk.js"
+  },
+  {
+    "revision": "42e537697a4d2967e961",
+    "url": "/React-ToDo/static/js/runtime-main.5b1cba61.js"
+  }
+]);
